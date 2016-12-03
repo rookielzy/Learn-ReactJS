4df5aac459fef9149968d4f13b4512e2835e34f1
unknown <>
2016-12-03 12:09:22 +0800

    To Componet & Lifecycle

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 #Learning How To Use ReactJS
+
+##JSX
+> JSX 是JS的扩展，拥有JS逻辑的同时还在JSX中可以加入 html语法。
+
+> 常见用法： [Details](https://facebook.github.io/react/docs/introducing-jsx.html)
+
+```JSX
+const element = <h1>Hello World</h1>
+const element = <h1>{someJS}</h1>
+const element = React.createElement(
+  'h1',
+  {className: 'greeting'},
+  'Hello World!'
+);
+```
+
+
+## Component and Lifecycle
+> this.state 必须使用 this.setState 来更新状态
+
+> State Update 有可能是异步的，所以 this.state 里的值更新应该分开来单独更新
+
+> 注意使用 componentDidMount & componentWillUnMount 来释放组件以此来销毁组件占用的资源。
--- src/index.js
+++ src/index.js
@@ -1,9 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+
+
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      date: new Date(),
+      text: ''
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  componentDidMount() {
+    setInterval(() => this.tick(), 1000);
+  }
+
+  tick() {
+    this.setState({
+      date: new Date()
+    });
+  }
+
+  handleChange(event) {
+    this.setState({text: event.target.value});
+  }
+
+  render() {
+    return (
+      <div>
+        <span>{this.state.date.toLocaleTimeString()}</span>
+        <input type="text" onChange={this.handleChange}/><span>{this.state.text}</span>
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(
-  <App />,
+  <Clock />,
   document.getElementById('root')
 );
