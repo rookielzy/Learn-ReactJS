772719039161babd1245f1eb985da5c2fe661a1b
rookielzy <slimjetlzy@gmail.com>
2017-08-08 16:11:25 +0800

    Fetching Data From Internet

--- Collapsible/src/js/App.js
+++ Collapsible/src/js/App.js
@@ -12,11 +12,33 @@ class App extends React.Component {
         this.fetchData = this.fetchData.bind(this)
     }
 
+    componentDidMount() {
+        this.fetchData()
+    }
+
     fetchData() {
         this.setState({
             isLoading: true,
             contacts: [],
         })
+
+        fetch('https://randomuser.me/api/?results=50&nat=us,dk,fr,gb')
+            .then(data => data.json())
+            .then(parsedJSON => parsedJSON.results.map(user => (
+                {
+                    name: `${user.name.first} ${user.name.last}`,
+                    username: `${user.login.username}`,
+                    email: `${user.email}`,
+                    location: `${user.location.street}, ${user.location.city}`,
+                }
+            )))
+            .then(contacts => {
+                this.setState({
+                    contacts: contacts,
+                    isLoading: false,
+                })
+            })
+            .catch(error => console.error(error))
     }
 
     render() {
@@ -28,9 +50,9 @@ class App extends React.Component {
                 </header>
                 <div className={`content ${this.state.isLoading ? "is-loading" : ""}`}>
                     <div className="panel-group">
-                        {!this.state.isLoading && this.state.contacts.length < 0 ? this.state.contacts.map(contact => {
-                            <Collapsible title="Test">
-                                <p>{contact}</p>
+                        {!this.state.isLoading && this.state.contacts.length > 0 ? this.state.contacts.map(contact => {
+                            return <Collapsible key={contact.username} title={contact.name}>
+                                <p>{contact.email}<br />{contact.location}</p>
                             </Collapsible>
                         }) : null}
                     </div>
