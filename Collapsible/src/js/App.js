322452d7c95ea32fc06e2d18ec9a19e96b4c1a13
rookielzy <slimjetlzy@gmail.com>
2017-08-08 15:11:25 +0800

    Loading

--- Collapsible/src/js/App.js
+++ Collapsible/src/js/App.js
@@ -1,26 +1,41 @@
 import React from 'react';
-import image from '../images/expand-vertical-4.svg';
+import image from '../images/cloud-upload-download-data-transfer.svg';
 import Collapsible from './Collapsible'
 
 class App extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            isLoading: false,
+            contacts: [],
+        }
+        this.fetchData = this.fetchData.bind(this)
+    }
+
+    fetchData() {
+        this.setState({
+            isLoading: true,
+            contacts: [],
+        })
+    }
+
     render() {
         return (
             <div>
                 <header>
                     <img src={image} />
-                    <h1>Collapsible Content</h1>
+                    <h1>Fetching Data <button className="btn btn-sm btn-danger" onClick={this.fetchData}>Fetch Now</button></h1>
                 </header>
-                <div className="content">
+                <div className={`content ${this.state.isLoading ? "is-loading" : ""}`}>
                     <div className="panel-group">
-                        <Collapsible title="Tracy Palmer">
-                            <p>tracey.palmer@example.com<br />3280 manchester road, ely</p>
-                        </Collapsible>
-                        <Collapsible title="Jade Dupuis">
-                            <p>jade.dupuis@example.com<br />1803 rue pasteur, nanterre</p>
-                        </Collapsible>
-                        <Collapsible title="Alfred Olsen">
-                            <p>alfred.olsen@example.com<br />6598 fjordparken, hirtsals</p>
-                        </Collapsible>
+                        {!this.state.isLoading && this.state.contacts.length < 0 ? this.state.contacts.map(contact => {
+                            <Collapsible title="Test">
+                                <p>{contact}</p>
+                            </Collapsible>
+                        }) : null}
+                    </div>
+                    <div className="loader">
+                        <div className="icon"></div>
                     </div>
                 </div>
             </div>
